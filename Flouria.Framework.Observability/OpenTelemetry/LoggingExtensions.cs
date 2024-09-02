b1dc29f00289ceb2f2089be06482b0782b550cbc
Ali Mahboubi <ali_mahboubi@outlook.com>
2024-09-02 14:32:45 +0330

    First Commit

---
+++ Flouria.Framework.Observability/OpenTelemetry/LoggingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using JobHunter.Framework.Observability.OpenTelemetry;
+using Serilog;
+using Serilog.Configuration;
+
+namespace Flouria.Framework.Observability.OpenTelemetry;
+
+public static class LoggingExtensions
+{
+    public static LoggerConfiguration WithOpenTelemetryTraceId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+    {
+        if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+        return enrichmentConfiguration.With<OpenTelemetryTraceIdEnricher>();
+    }
+    
+    public static LoggerConfiguration WithOpenTelemetrySpanId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+    {
+        if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+        return enrichmentConfiguration.With<OpenTelemetrySpanIdEnricher>();
+    }
+}
